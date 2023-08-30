238b5d4e13ab2f53107400adc1a316f994d802e3
Jack Cavanagh <jack.m.cavanagh@hotmail.co.uk>
2023-08-30 15:36:42 +0100

    Added Solution for FunctionQuestions: A2a

--- Python_Basics/Exercizes/FunctionQuestions.py
+++ Python_Basics/Exercizes/FunctionQuestions.py
@@ -36,15 +36,24 @@ def are_factors(num1, num2):
 
 
 print(are_factors(11, 34))
+
 # -------------------------------------------------------------------------------------- #
 
 print("\nQ2a\n")
 # Q2a: write a function which takes a letter (as a string) as an input and outputs it's position in the alphabet
-# alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-#             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
 
 # A2a:
+def letter_position(letter):
+    if type(letter) != type("string"):
+        print("Error Input Must Be String")
+    elif len(letter) > 1:
+        print("Error Input Must Only Be Single Letter")
+    else:
+        return alphabet.index(letter)+1
 
+print(letter_position("r"))
 
 print("\nQ2b\n")
 # Q2b: create a function which takes a persons name as an input string and returns an
