b44a4d3b971b85dc81765915b6a2a700bda44759
Jack Cavanagh <jack.m.cavanagh@hotmail.co.uk>
2023-08-30 15:20:16 +0100

    Added Solution for FunctionQuestions: A1b

--- Python_Basics/Exercizes/FunctionQuestions.py
+++ Python_Basics/Exercizes/FunctionQuestions.py
@@ -1,4 +1,6 @@
 print("\nQ1a\n")
+
+
 # Q1a: Write a function which takes in an integer as an argument and returns the divisors of that number as a list
 # e.g. f(12) = [1, 2, 3, 4, 6, 12]
 # hint: range(1, n) returns a collection of the numbers from 1 to n-1
@@ -6,23 +8,34 @@ print("\nQ1a\n")
 # A1a:
 def find_divisors(number):
     divisors = []
-    for n in range(1, number):
+    for n in range(1, number + 1):
         if number % n == 0:
             divisors.append(n)
     return divisors
 
+
 print(find_divisors(32))
 
-
 print("\nQ1b\n")
+
+
 # Q1b: Write a function which takes in two integers as arguments and returns true if one of the numbers
 # is a factor of the other, false otherwise
 # (bonus points if you call your previous function within this function
 
 # A1b:
 
+def are_factors(num1, num2):
+    numbers = [num1, num2]
+    divisors = find_divisors(max(numbers))
+    for n in divisors:
+        if min(numbers) == n:
+            return True
+        else:
+            return False
 
 
+print(are_factors(11, 34))
 # -------------------------------------------------------------------------------------- #
 
 print("\nQ2a\n")
@@ -33,14 +46,12 @@ print("\nQ2a\n")
 # A2a:
 
 
-
 print("\nQ2b\n")
 # Q2b: create a function which takes a persons name as an input string and returns an
 # ID number consisting of the positions of each letter in the name
 # e.g. f("bob") = "1141" as "b" is in position 1 and "o" is in position 14
 
 # A2b:
-
 
 
 print("\nQ2c\n")
@@ -51,7 +62,6 @@ print("\nQ2c\n")
 # A2c:
 
 
-
 # -------------------------------------------------------------------------------------- #
 
 print("\nQ3a\n")
@@ -60,18 +70,10 @@ print("\nQ3a\n")
 # A3a:
 
 
-
 print("\nQ3b\n")
 # Q3b: Now add some functionality to the function which does not error if the user inputs something other than a digit
 
 # A3b:
 
 
-
 # -------------------------------------------------------------------------------------- #
-
-
-
-
-
-
