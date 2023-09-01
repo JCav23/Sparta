7add6cfb8d37d1079c94ed879315d681971fa470
Jack Cavanagh <jack.m.cavanagh@hotmail.co.uk>
2023-09-01 16:41:34 +0100

    added solution for OOP Classes Questions A3a

--- PythonOOP/OOP Questions/ClassesQuestions.py
+++ PythonOOP/OOP Questions/ClassesQuestions.py
@@ -89,29 +89,29 @@ print("\nQ3a\n")
 # Q3a: Fix the following class and subclass (uncomment by selecting all rows and pressing CTRL + /)
 
 
-# class Boss(object):
-#     def __init__(self, name, attitude, behaviour, face):
-#         name = name
-#         attitude = attitude
-#         behaviour = behaviour
-#         face = face
-#
-#     def get_attitude(self):
-#         return attitude
-#
-#     def get_behaviour(self):
-#         return behaviour
-#
-#     def get_face(self):
-#         return face
-#
-#
-# class GoodBoss(Boss):
-#     def __init__(self, name, attitude, behaviour, face):
-#         super()
-#
-#    def encourage(self):
-#        print(f"The team cheers for {self.name}, starts shouting awesome slogans then gets back to work.")
+class Boss(object):
+    def __init__(self, name, attitude, behaviour, face):
+        self.name = name
+        self.attitude = attitude
+        self.behaviour = behaviour
+        self.face = face
+
+    def get_attitude(self):
+        return self.attitude
+
+    def get_behaviour(self):
+        return self.behaviour
+
+    def get_face(self):
+        return self.face
+
+
+class GoodBoss(Boss):
+    def __init__(self, name, attitude, behaviour, face):
+        super().__init__(name, attitude, behaviour, face)
+
+    def encourage(self):
+       print(f"The team cheers for {self.name}, starts shouting awesome slogans then gets back to work.")
 
 
 # A3a:
