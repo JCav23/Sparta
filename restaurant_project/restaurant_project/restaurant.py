80d00d72637525d83f336f3582377b3ad8fc54e8
Jack Cavanagh <jack.m.cavanagh@hotmail.co.uk>
2023-09-11 15:04:57 +0100

    added class init and order method to restaurant

--- restaurant_project/restaurant_project/restaurant.py
+++ restaurant_project/restaurant_project/restaurant.py
@@ -1,2 +1,10 @@
-class Table(object):
-    def __init__(self, ):
+class Table:
+    def __init__(self, diners):
+        self.diners = diners
+
+    def bill(self):
+        return self.bill()
+
+    @classmethod
+    def order(cls, item=str, price=float, quantity=(int, 1)):
+        cls.bill.append = [{'item': item, 'price': price, 'quantity': quantity}]
